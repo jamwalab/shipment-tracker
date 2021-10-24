5bcf73014034d30dd0e882206bde14894aa29230
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-10-24 15:21:07 -0400

    New data form added

--- client/src/components/CustomerDropdown/index.js
+++ client/src/components/CustomerDropdown/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {FormControl, Select, MenuItem } from '@mui/material';
 
@@ -10,18 +10,28 @@ export default function CustomerDropdown(client) {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  const handleChange = (rowId) => (event) => {
-    let index = state.savedCustomers.findIndex(ele => ele.customer_name === event.target.value);
-    
+  const [menuValue, setMenuValue] = useState("");
+  
+  useEffect (() =>{
+    setMenuValue(client.selectedRow);
+  }, [])
 
-    const dataUpload = {"customer_id": state.savedCustomers[index].id}
-/////---FOR SERVER SAVE IT HAS TO BE CUSTOMER ID
-    serverApiCall.editShipments(dataUpload, rowId)
-    console.log(dataUpload, rowId)
-    dispatch({
-      type: EDIT_SHIPMENT,
-      editData: {rowId, col: "customer", data: event.target.value}
-    })
+  const handleChange = (rowId) => (event) => {
+    //Find index in saved customer list in global state
+    let index = state.savedCustomers.findIndex(ele => ele.customer_name === event.target.value);
+    setMenuValue(event.target.value);
+    //Use the index to find the customer id
+    if (index!== -1) {
+      const dataUpload = {"customer_id": state.savedCustomers[index].id}
+      //---FOR SERVER SAVE IT HAS TO BE CUSTOMER ID
+      serverApiCall.editShipments(dataUpload, rowId)
+      console.log(dataUpload, rowId)
+      //edit customer in global state
+      dispatch({
+        type: EDIT_SHIPMENT,
+        editData: {rowId, col: "customer", data: event.target.value}
+      })
+    }
   }
 
   return (
@@ -30,7 +40,7 @@ export default function CustomerDropdown(client) {
       <Select
         id="demo-simple-select-standard"
         label="Customer"
-        defaultValue={client.selectedRow}
+        value={menuValue}
         onChange={handleChange(client.rowId)}
       >
         {state.savedCustomers.map((customer) => {
--- client/src/components/NewShipmentForm/index.js
+++ client/src/components/NewShipmentForm/index.js
@@ -1,35 +1,167 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from "react-redux";
 
 //---MATERIAL UI IMPORTS---//
-import {Box, Input, IconButton, FilledInput, OutlinedInput, InputLabel, InputAdornment, FormHelperText, FormControl, TextField} from '@mui/material';
+import {Box, Input, IconButton, FilledInput, OutlinedInput, InputLabel, InputAdornment, FormHelperText, FormControl, TextField, TextareaAutosize, Select, MenuItem, Button} from '@mui/material';
+import {ThemeProvider, createTheme } from '@mui/material';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+
+import './style.css';
+
+const theme = createTheme({
+
+});
 
 const NewShipmentForm = () => {
+  //---GLOBAL STATE---//
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const [formData, updateFormData] = useState({
+    job: "",
+    customer: "",
+    notes: "",
+    po: "",
+    bl: "",
+    subL: "",
+    portEta: null,
+    destEta: null,
+    submitted: null
+  })
+
+  const handleDateChange = (type, newDate) => {
+    updateFormData({ ...formData, [type]: newDate });
+    console.log(formData)
+  }
+
+  const handleChange = (prop) => (event) => {
+    updateFormData({ ...formData, [prop]: event.target.value });
+    console.log(formData)
+  };
+
+  const handleSubmit = () => {
+
+  }
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: "center", flexDirection:"column", alignItems:"center"}}>
-      <h2>NEW SHIPMENT DETAILS</h2>
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="standard-adornment-job">Job#</InputLabel>
-        <Input
-          id="standard-adornment-job"
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex', justifyContent: "center", flexDirection:"column", alignItems:"center"}}>
+        <h2>NEW SHIPMENT DETAILS</h2>
+        <div>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="standard-adornment-job" sx={{ mx: 2}}>Job#</InputLabel>
+            <Input
+              id="standard-adornment-job"
+              value={formData.job}
+              onChange={handleChange('job')}
+              sx={{ m: 1, width: '400px'}}
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel id="demo-simple-select-standard-label" sx={{ mx: 3}}>Customer</InputLabel>
+            <Select
+              id="demo-simple-select-standard"
+              labelId="demo-simple-select-standard-label"
+              label="Customer"
+              value={formData.customer}
+              onChange={handleChange('customer')}
+              sx={{ m: 1, width: '400px'}}
+            >
+              {state.savedCustomers.map((customer) => {
+                return (
+                  <MenuItem value={customer.customer_name} key={customer.customer_name}>{customer.customer_name}</MenuItem>
+              )})}
+            </Select>
+          </FormControl>
+        </div>
+        <TextField
+          placeholder="NOTES"
+          multiline
+          rows={2}
           sx={{ m: 2, width: '800px'}}
         />
-      </FormControl>
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="standard-adornment-po">PO</InputLabel>
-        <Input
-          id="standard-adornment-po"
-          sx={{ m: 2, width: '800px'}}
-        />  
-      </FormControl>
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="standard-adornment-bl">BL</InputLabel>
-        <Input
-          id="standard-adornment-bl"
-          sx={{ m: 2, width: '800px'}}
-        />  
-      </FormControl>
-    </Box>
+        <div>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="standard-adornment-po" sx={{ mx: 2}}>PO</InputLabel>
+            <Input
+              id="standard-adornment-po"
+              sx={{ m: 2, width: '250px'}}
+            />  
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="standard-adornment-bl" sx={{ mx: 2}}>BL</InputLabel>
+            <Input
+              id="standard-adornment-bl"
+              sx={{ m: 2, width: '250px'}}
+            />  
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="standard-adornment-job" sx={{ mx: 2}}>SubL</InputLabel>
+            <Input
+              id="standard-adornment-job"
+              sx={{ m: 2, width: '250px'}}
+            />
+          </FormControl>
+        </div>
+        <TextField
+          placeholder="CONTAINERS"
+          multiline
+          rows={2}
+          sx={{ m: 1, width: '800px'}}
+        />
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDateFns} >
+            <DatePicker
+              label="Port ETA"
+              value={formData.portEta}
+              onChange={(newValue) => {
+                handleDateChange("portEta", newValue)
+              }}
+              /*{(newValue) => {
+                setDateValue(newValue);
+              }}*/
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns} >
+            <DatePicker
+              label="Destination ETA"
+              value={formData.destEta}
+              onChange={(newValue) => {
+                handleDateChange("destEta", newValue)
+              }}
+              /*{(newValue) => {
+                setDateValue(newValue);
+              }}*/
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns} >
+            <DatePicker
+              label="Submitted"
+              value={formData.submitted}
+              onChange={(newValue) => {
+                handleDateChange("submitted", newValue)
+              }}
+              /*{(newValue) => {
+                setDateValue(newValue);
+              }}*/
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+        </div>
+        <TextField
+          placeholder="COMMENTS"
+          multiline
+          rows={2}
+          sx={{ m: 1, width: '800px'}}
+        />
+        <p className="errorText">Customer name is required. Please try again!!</p>
+        <Button variant="contained" onSubmit={handleSubmit}>SUBMIT</Button>
+      </Box>
+    </ThemeProvider>
   )
 }
 
